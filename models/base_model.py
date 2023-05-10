c65c6d7c49c19c413cb603558db44e6b13badd86
Birhan Abuhay Jemere <124788842+TgGeda@users.noreply.github.com>
2023-05-10 22:16:41 +0300

    I am Updating  base_model.py class

--- models/base_model.py
+++ models/base_model.py
@@ -1,1 +1,25 @@
+!#/usr/bin/python3
 
+class BaseModel:
+    """Defines all common attributes/methods for other classes"""
+    def __init__(self):
+        """Initialize public instance attributes"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """Return string representation of object"""
+        return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
+
+    def save(self):
+        """Update public instance attribute updated_at with current datetime"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Return dictionary representation of object"""
+        dict_copy = self.__dict__.copy()
+        dict_copy['__class__'] = type(self).__name__
+        dict_copy['created_at'] = self.created_at.isoformat()
+        dict_copy['updated_at'] = self.updated_at.isoformat()
+        return dict_copy
