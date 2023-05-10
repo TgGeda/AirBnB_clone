22a92236c1b6e83aebebc565321bfd032d69e61e
Birhan Abuhay Jemere <124788842+TgGeda@users.noreply.github.com>
2023-05-10 13:47:20 +0300

    Update file_storage.py to hold its task

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,1 +1,30 @@
+#!/usr/bin/python3
+"""Defines the FileStorage class."""
+import json
 
+class FileStorage:
+  """Represent an abstracted storage engine.
+    Attributes:
+        __file_path (str): The name of the file to save objects to.
+        __objects (dict): A dictionary of instantiated objects.
+    """
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        return self.__objects
+
+    def new(self, obj):
+        key = obj.__class__.__name__ + '.' + obj.id
+        self.__objects[key] = obj
+
+    def save(self):
+        with open(self.__file_path, 'w') as file:
+            json.dump(self.__objects, file)
+
+    def reload(self):
+        try:
+            with open(self.__file_path, 'r') as file:
+                self.__objects = json.load(file)
+        except FileNotFoundError:
+            return
