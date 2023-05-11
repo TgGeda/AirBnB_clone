d280f64eb1a92111dbb06db51f2ad93adda43517
Birhan Abuhay Jemere <124788842+TgGeda@users.noreply.github.com>
2023-05-11 10:54:07 +0300

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """__init__ magic method for models directory. 
-The code above is a Python script that initializes the "__init__" magic method for the models directory. 
+The code below is a Python script that initializes the "__init__" magic method for the models directory. 
 It imports the "FileStorage" class from the "models.engine.file_storage" module. 
 
 Then, it creates an instance of the "FileStorage" class and assigns it to the variable "storage".
