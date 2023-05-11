ac59a9564d102ef7e3951555b8448e4c11674a34
Birhan Abuhay Jemere <124788842+TgGeda@users.noreply.github.com>
2023-05-11 12:22:35 +0300

    I am linking  BaseModel to FileStorage by using the variable storage through Updating base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,12 +1,21 @@
 !#/usr/bin/python3
-
+import models
 class BaseModel:
     """Defines all common attributes/methods for other classes"""
-    def __init__(self):
+    def __init__(self, *args , *kwargs):
         """Initialize public instance attributes"""
+        time_format = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    self.__dict__[k] = datetime.strptime(v, time_format)
+                else:
+                    self.__dict__[k] = v
+        else:
+            models.storage.new(self)
 
     def __str__(self):
         """Return string representation of object"""
@@ -15,6 +24,8 @@ class BaseModel:
     def save(self):
         """Update public instance attribute updated_at with current datetime"""
         self.updated_at = datetime.now()
+        models.storage.save()
+
 
     def to_dict(self):
         """Return dictionary representation of object"""
