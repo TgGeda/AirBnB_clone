a868fbf9838cce6e70cbdc5c8e1ac8a61467e2a1
Birhan Abuhay Jemere <124788842+TgGeda@users.noreply.github.com>
2023-05-13 18:40:31 +0300

    Update file_storage.py to include serialization

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,8 +8,10 @@ from models.city import City
 from models.place import Place
 from models.amenity import Amenity
 from models.review import Review
+
+
 class FileStorage:
-  """Represent an abstracted storage engine.
+    """Represent an abstracted storage engine.
     Attributes:
         __file_path (str): The name of the file to save objects to.
         __objects (dict): A dictionary of instantiated objects.
@@ -18,19 +20,29 @@ class FileStorage:
     __objects = {}
 
     def all(self):
+        """Return the dictionary __objects."""
         return self.__objects
 
     def new(self, obj):
-        key = obj.__class__.__name__ + '.' + obj.id
+        """Set in __objects obj with key <obj_class_name>.id"""
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
         self.__objects[key] = obj
 
     def save(self):
-        with open(self.__file_path, 'w') as file:
-            json.dump(self.__objects, file)
+        """Serialize __objects to the JSON file __file_path."""
+        objdict = {key: value.to_dict() for key, value in self.__objects.items()}
+        with open(self.__file_path, "w") as f:
+            json.dump(objdict, f)
 
     def reload(self):
+        """Deserialize the JSON file __file_path to __objects, if it exists."""
         try:
-            with open(self.__file_path, 'r') as file:
-                self.__objects = json.load(file)
+            with open(self.__file_path) as f:
+                objdict = json.load(f)
+                for o in objdict.values():
+                    if '__class__' in o:
+                        cls = eval(o['__class__'])
+                        del o['__class__']
+                        self.new(cls(**o))
         except FileNotFoundError:
-            return
+            pass
