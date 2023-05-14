b158ffdace50b7dee2700d06f81ed3793cb15256
Birhan Abuhay Jemere <124788842+TgGeda@users.noreply.github.com>
2023-05-14 18:55:05 +0300

    Update test_review.py that Defines unittests for models/review.py

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Defines unittests for models/review.py.
+
 Unittest classes:
     TestReview_instantiation
     TestReview_save
@@ -198,4 +199,3 @@ class TestReview_to_dict(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
-
