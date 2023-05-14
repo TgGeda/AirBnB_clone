106441666b2df5d239b8f4a9fb1e1ad7e316a688
Birhan Abuhay Jemere <124788842+TgGeda@users.noreply.github.com>
2023-05-14 18:08:35 +0300

    Update state.py class that shows a state for user

--- models/state.py
+++ models/state.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
-"""Defines the State class.A class representing a state that inherits from BaseModel."""
+"""This module creates a User class"""
+
 from models.base_model import BaseModel
 
 
 class State(BaseModel):
-    """
-    Public class attributes:
-
-    name: A string representing the name of the state. 
-    Default is an empty string.
-    """
+    """Class for managing state objects"""
 
     name = ""
