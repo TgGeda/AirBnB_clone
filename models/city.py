0ce78196f3f6c09d0893f4a966f3a57f34c9c4d2
Birhan Abuhay Jemere <124788842+TgGeda@users.noreply.github.com>
2023-05-14 18:05:11 +0300

    Update city.py class that shows a city option

--- models/city.py
+++ models/city.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
-"""Defines the City class.A class representing a city that inherits from BaseModel."""
+"""This module creates a User class"""
+
 from models.base_model import BaseModel
 
 
 class City(BaseModel):
-    """Represent a city.
-    Public class attributes:
-    state_id: string - empty string: it will be the State.id
-    name: string - empty string: name of the city
-    """
+    """Class for managing city objects"""
 
     state_id = ""
     name = ""
