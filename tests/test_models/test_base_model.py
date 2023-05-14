021e03c1b97a99bc357bf9ba47077d7d60c64b0b
Birhan Abuhay Jemere <124788842+TgGeda@users.noreply.github.com>
2023-05-14 18:51:43 +0300

    Update test_base_model.py that Defines unittests for models/base_model.py

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Defines unittests for models/base_model.py.
+
 Unittest classes:
     TestBaseModel_instantiation
     TestBaseModel_save
@@ -189,4 +190,3 @@ class TestBaseModel_to_dict(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
-
